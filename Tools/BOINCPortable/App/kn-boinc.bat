b9c4db76821309adc210d2917b4729b656ac4f22
TechnoRat55 <technorat55@gmx.de>
2017-04-02 18:32:53 +0200

    updates for new boinc version

--- Tools/BOINCPortable/App/kn-boinc.bat
+++ Tools/BOINCPortable/App/kn-boinc.bat
@@ -1,5 +1,21 @@
 @echo off
 
-cd %~dp0\DefaultData\_work\
-SET BOIN="%~dp0\BOINC\boincmgr.exe"
-%BOIN%
+SET TMP_USERPROFILE=%~dp0\..\Data\USERPROFILE
+SET TMP_HOMEPATH=%~p0\..\Data\USERPROFILE
+
+rem ##### resolve relative paths and set environment #####
+pushd "%TMP_USERPROFILE%"
+set USERPROFILE=%CD%
+popd
+
+pushd "%TMP_HOMEPATH%"
+set HOMEPATH=%CD:~2%
+popd
+
+set HOMEDRIVE="%~d0"
+
+SET
+PAUSE
+
+%~dp0\BOINC\boincmgr.exe
+exit
